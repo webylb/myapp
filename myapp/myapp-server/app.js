a487a18fbdd110ff651a3ead1b88a2febdeec2eb
webylb <1475452561@qq.com>
2019-05-09 09:06:32 +0800

    :art:first commit

---
+++ myapp/myapp-server/app.js
@@ -0,0 +1,74 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+// mongodb
+var MongoClient = require('mongodb').MongoClient;
+var dbURL = "mongodb://localhost:27017";
+
+
+app.listen(80);
+
+app.get('/hello', (req, res) => {
+    
+    MongoClient.connect(dbURL, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("demo");
+	    dbo.collection("user"). find({}).toArray(function(err, result) { // 返回集合中所有数据
+	        if (err) throw err;
+	        res.send(result)
+	        db.close();
+	    });
+	});
+    
+})
+
+app.get('/world', function(req, res) { 
+	res.sendFile(__dirname + '/views/world.html');
+})
+
+app.get('/', function(req, res) { 
+	res.sendFile(__dirname + '/views/index.html');
+})
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
